5a24114767a57c1dac3e31ff31b334d3e63d91ca
Zac Godinez <zac.godinez@tutanota.com>
2023-07-30 12:48:13 +1000

    blog strip

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,19 +1,18 @@
-import { component$ } from '@builder.io/qwik';
-import { useServerTimeLoader } from '~/routes/layout';
-import styles from './footer.module.css';
+import { component$ } from "@builder.io/qwik";
+import { useServerTimeLoader } from "~/routes/layout";
+// import styles from './footer.module.css';
 
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
+	const serverTime = useServerTimeLoader();
 
-  return (
-    <footer>
-      <div class="container">
-        <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-          <span>Made with ♡ by Builder.io</span>
-          <span class={styles.spacer}>|</span>
-          <span>{serverTime.value.date}</span>
-        </a>
-      </div>
-    </footer>
-  );
+	return (
+		<footer>
+			<div class="container">
+				<a href="https://www.builder.io/" target="_blank">
+					<span>Made with ♡ by Builder.io</span>
+					<span>{serverTime.value.date}</span>
+				</a>
+			</div>
+		</footer>
+	);
 });
