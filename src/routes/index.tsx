feede8f0147f8eb3eb76fd2ca160ac30433f46c1
Zac Godinez <zac.godinez@tutanota.com>
2023-07-30 12:37:45 +1000

    copied posts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,6 +12,14 @@ export default component$(() => {
 			<h1 class={styles.blueClass}>
 				Welcome to Qwik <span class="lightning">⚡️</span>
 			</h1>
+			<h2 class="text-xl sm:text-2xl font-bold leading-snug mb-2 font-heading">
+				<a
+					class="hover:text-primary-600 underline underline-offset-4 decoration-1 decoration-dotted transition ease-in duration-200"
+					href={`/posts`}
+				>
+					Posts
+				</a>
+			</h2>
 			<h1 class="font-sans text-4xl font-bold">testing Inter font</h1>
 
 			<div class="w-full ">
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,16 @@ module.exports = {
 			},
 			fontFamily: {
 				sans: ["'Inter'", ...defaultTheme.fontFamily.sans],
+				//  sans: ["Mona Sans", "sans-serif"],
+				//         heading: ["Hubot Sans", "sans-serif"],
+				//         mono: ["JetBrains Mono", "monospace"],
+			},
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
 			},
 		},
 	},
-	plugins: [require("@tailwindcss/typography")],
+	plugins: [require("@tailwindcss/typography"), require("tailwind-scrollbar")],
 	darkMode: "class",
+	important: true,
 };
