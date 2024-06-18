4830c9c7b6c906f3d11dabb43752fcbf429e2ff2
Zac Godinez <zac.godinez@tutanota.com>
2024-06-18 22:40:14 +1000

    little bit more testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,7 @@ const whenExternalScripts = (items = []) =>
 export default defineConfig({
   site: 'https://zacgodinez.github.io',
   base: '/z-god-site',
-  // If the website uses trailing slashes in the URLs
-  // trailingSlash: false,
-  // output: 'static',
+  output: 'static',
   integrations: [
     tailwind({
       applyBaseStyles: false,
