31caf779b7e41d6495b40fe505fee863193c7746
Zac Godinez <zac.godinez@tutanota.com>
2024-06-18 20:43:42 +1000

    update readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,8 @@ const whenExternalScripts = (items = []) =>
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://zacgodinez.github.io',
+  base: 'z-god-site',
   output: 'static',
 
   integrations: [
