8d8d76779de967c0e920364b29cfaec53324af07
Zac Godinez <zac.godinez998463646343@gmail.com>
2024-10-14 18:46:17 +1100

    working on three js int

--- src/scripts/webgl/TCanvas.ts
+++ src/scripts/webgl/TCanvas.ts
@@ -1,35 +1,35 @@
-import * as THREE from 'three'
-import { gl } from './core/WebGL'
-import { Assets, loadAssets } from './utils/assetLoader'
-import { controls } from './utils/OrbitControls'
-import vertexShader from './shader/vs.glsl'
-import fragmentShader from './shader/fs.glsl'
-import { calcCoveredTextureScale } from './utils/coveredTexture'
+import * as THREE from 'three';
+import { gl } from './core/WebGL';
+import { Assets, loadAssets } from './utils/assetLoader';
+import { controls } from './utils/OrbitControls';
+import vertexShader from './shader/vs.glsl';
+import fragmentShader from './shader/fs.glsl';
+import { calcCoveredTextureScale } from './utils/coveredTexture';
 
 export class TCanvas {
   private assets: Assets = {
     image: { path: 'images/unsplash.jpg' },
-  }
+  };
 
   constructor(private container: HTMLElement) {
     loadAssets(this.assets).then(() => {
-      this.init()
-      this.createObjects()
-      gl.requestAnimationFrame(this.anime)
-    })
+      this.init();
+      this.createObjects();
+      gl.requestAnimationFrame(this.anime);
+    });
   }
 
   private init() {
-    gl.setup(this.container)
-    gl.scene.background = new THREE.Color('#012')
-    gl.camera.position.z = 1.5
+    gl.setup(this.container);
+    gl.scene.background = new THREE.Color('#012');
+    gl.camera.position.z = 1.5;
   }
 
   private createObjects() {
-    const texture = this.assets.image.data as THREE.Texture
-    const scale = calcCoveredTextureScale(texture, 1 / 1)
+    const texture = this.assets.image.data as THREE.Texture;
+    const scale = calcCoveredTextureScale(texture, 1 / 1);
 
-    const geometry = new THREE.PlaneGeometry(1, 1)
+    const geometry = new THREE.PlaneGeometry(1, 1);
     const material = new THREE.ShaderMaterial({
       uniforms: {
         tImage: { value: texture },
@@ -38,22 +38,22 @@ export class TCanvas {
       vertexShader,
       fragmentShader,
       side: THREE.DoubleSide,
-    })
-    const mesh = new THREE.Mesh(geometry, material)
+    });
+    const mesh = new THREE.Mesh(geometry, material);
 
-    gl.scene.add(mesh)
+    gl.scene.add(mesh);
   }
 
   // ----------------------------------
   // animation
   private anime = () => {
-    controls.update()
-    gl.render()
-  }
+    controls.update();
+    gl.render();
+  };
 
   // ----------------------------------
   // dispose
   dispose() {
-    gl.dispose()
+    gl.dispose();
   }
 }
--- src/scripts/webgl/utils/coveredTexture.ts
+++ src/scripts/webgl/utils/coveredTexture.ts
@@ -1,19 +1,20 @@
+import * as THREE from 'three';
 export function calcCoveredTextureScale(texture: THREE.Texture, aspect: number, target?: THREE.Vector2) {
-  const imageAspect = texture.image.width / texture.image.height
+  const imageAspect = texture.image.width / texture.image.height;
 
-  let result: [number, number] = [1, 1]
-  if (aspect < imageAspect) result = [aspect / imageAspect, 1]
-  else result = [1, imageAspect / aspect]
+  let result: [number, number] = [1, 1];
+  if (aspect < imageAspect) result = [aspect / imageAspect, 1];
+  else result = [1, imageAspect / aspect];
 
-  target?.set(result[0], result[1])
+  target?.set(result[0], result[1]);
 
-  return result
+  return result;
 }
 
 export function coveredTexture(texture: THREE.Texture, screenAspect: number) {
-  texture.matrixAutoUpdate = false
-  const scale = calcCoveredTextureScale(texture, screenAspect)
-  texture.matrix.setUvTransform(0, 0, scale[0], scale[1], 0, 0.5, 0.5)
+  texture.matrixAutoUpdate = false;
+  const scale = calcCoveredTextureScale(texture, screenAspect);
+  texture.matrix.setUvTransform(0, 0, scale[0], scale[1], 0, 0.5, 0.5);
 
-  return texture
+  return texture;
 }
